3073f1f0e930f8c89be0634a769a5121743c1e04
Iqbal Atmojo <iqbaladmojoekasandi@gmail.com>
2025-05-17 10:37:23 +0700

    menambahakan halaman login,register,beranda

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -1,7 +1,13 @@
 import 'package:get/get.dart';
 
+import '../modules/beranda/bindings/beranda_binding.dart';
+import '../modules/beranda/views/beranda_view.dart';
 import '../modules/home/bindings/home_binding.dart';
 import '../modules/home/views/home_view.dart';
+import '../modules/login/bindings/login_binding.dart';
+import '../modules/login/views/login_view.dart';
+import '../modules/register/bindings/register_binding.dart';
+import '../modules/register/views/register_view.dart';
 
 part 'app_routes.dart';
 
@@ -15,6 +21,21 @@ class AppPages {
       name: _Paths.HOME,
       page: () => const HomeView(),
       binding: HomeBinding(),
+    ),
+    GetPage(
+      name: _Paths.LOGIN,
+      page: () => const LoginView(),
+      binding: LoginBinding(),
+    ),
+    GetPage(
+      name: _Paths.REGISTER,
+      page: () => const RegisterView(),
+      binding: RegisterBinding(),
+    ),
+    GetPage(
+      name: _Paths.BERANDA,
+      page: () => const BerandaView(),
+      binding: BerandaBinding(),
     ),
   ];
 }
